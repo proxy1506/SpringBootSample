9edd89e9377f02e56e0925035778732358cf535e
proxy1506 <43613361+proxy1506@users.noreply.github.com>
2019-09-21 15:03:05 -0400

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - v3! Testing Dockerizing Jenkins pipeine";
+        return "Hello World - v3! Testing Dockerizing Jenkins pipeine 1";
     }
 
     public static void main(String[] args) throws Exception {
